426fd822776e1ea51eeea9aa1b1a132eafacca31
Waryono <56479765+WA4YON9-ID-cyber@users.noreply.github.com>
2020-01-16 17:39:43 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Masukan user kaliaan
+$user = "22397";
+
+// Masukan key kaliaan
+$key = "f092e3f9e0eea1a86810ec18bf859beaaa5de01e";
+
+/* Jangan Edit Script bot ,
+   jika di edit akan error 
+*/
+
+?>
